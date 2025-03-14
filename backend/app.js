2fb3c3f4b247fff026e955b64445562106e9b6de
ariix00 <armandoferra2685@gmail.com>
2025-03-14 20:43:21 -0300

    basic server created on backend folder

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const fileSys = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+const methodOverride = require("method-override");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

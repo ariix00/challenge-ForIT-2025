b9c7f6accb0838158efd8dcde1f62cbc2d5ec79b
ariix00 <armandoferra2685@gmail.com>
2025-03-17 19:44:32 -0300

    feat: add endpoints and functions. chore: created .env and added enviroment variables

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const fileSys = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const cors = require("cors");
 const methodOverride = require("method-override");
 
@@ -12,10 +13,61 @@ app.use(express.urlencoded());
 app.use(cors());
 app.use(methodOverride("_method"));
 
+const tasksPath = path.join(__dirname, process.env.TASKS_PATH);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.get("/api/tasks", (req, res) => {
+  const taskList = fileSys.readFileSync(tasksPath, "utf-8");
+  res.send(taskList);
+  console.log(taskList);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+app.post("/api/tasks", (req, res) => {
+  const taskList = JSON.parse(fileSys.readFileSync(tasksPath, "utf-8"));
+  const newTask = {
+    id: taskList[taskList.length - 1].id + 1,
+    title: req.body.title,
+    description: req.body.description,
+    completed: false,
+  };
+
+  console.log(taskList);
+  taskList.push(newTask);
+  fileSys.writeFileSync(
+    tasksPath,
+    JSON.stringify(taskList, null, " "),
+    "utf-8"
+  );
+
+  res.redirect("/");
+});
+app.put("/api/tasks/:id", (req, res) => {
+  const taskList = JSON.parse(fileSys.readFileSync(tasksPath, "utf-8"));
+  const editedTask = taskList.find((task) => task.id == req.params.id);
+  editedTask.description = req.body.description;
+  editedTask.title = req.body.title;
+
+  fileSys.writeFileSync(
+    tasksPath,
+    JSON.stringify(taskList, null, " "),
+    "utf-8"
+  );
+  res.redirect("/");
+});
+app.delete("api/tasks/:id", (req, res) => {
+  console.log(req.params.id);
+  const taskList = JSON.parse(fileSys.readFileSync(tasksPath, "utf-8"));
+  const filteredTask = taskList.filter((task) => {
+    return task.id != req.params.id;
+  });
+  fileSys.writeFileSync(
+    tasksPath,
+    JSON.stringify(filteredTask, null, " "),
+    "utf-8"
+  );
+  res.redirect("/");
 });

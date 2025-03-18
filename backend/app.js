2e1e09faddaace31b55530bc0b34debd850f67fa
ariix00 <armandoferra2685@gmail.com>
2025-03-17 22:55:15 -0300

    feat: added the fetch of the api

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT;
 const cors = require("cors");
-const methodOverride = require("method-override");
 
-app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
-app.use(methodOverride("_method"));
 
 const tasksPath = path.join(__dirname, process.env.TASKS_PATH);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/", (req, res) => {});
 app.get("/api/tasks", (req, res) => {
   const taskList = fileSys.readFileSync(tasksPath, "utf-8");
   res.send(taskList);
--- frontend/src/pages/TaskList.tsx
+++ frontend/src/pages/TaskList.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface Task {
+  id: string;
+  title: string;
+  description: string;
+  completed: boolean;
+}
 const TaskList = () => {
+  const api = import.meta.env.VITE_API;
+  const [taskList, setTaskList] = useState<Task[]>();
+  useEffect(() => {
+    (async function () {
+      try {
+        const data = await fetch(api);
+        const response = await data.json();
+        console.log(response);
+        setTaskList(response);
+      } catch (error) {
+        console.log("error fetching data: ", error);
+      }
+    })();
+  }, [api]);
   return (
     <div className="h-[750px] max-w-[600px] bg-zinc-500 flex flex-wrap p-5 gap-5">
-      <div className="w-56 bg-slate-50 h-5 "></div>
+      {taskList ? (
+        taskList?.map((task) => {
+          return (
+            <>
+              <div className="w-56 bg-slate-50 h-5">
+                <h1>{task.title}</h1>
+                <span>{task.description}</span>
+              </div>
+            </>
+          );
+        })
+      ) : (
+        <div>No hay tareas!</div>
+      )}
+
       <div className="w-56 bg-slate-50 h-5 "></div>
     </div>
   );
